738f090c47af3075e75bacf8af5c68628a936bc3
AdarshGrewal <adarsh@aosip.dev>
2021-05-12 13:52:44 +0530

    sweet: drop prebuilt kernel modules

--- sweet-vendor.mk
+++ sweet-vendor.mk
@@ -150,46 +150,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sweet/proprietary/system_ext/lib64/vendor.qti.ims.factory@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.ims.factory@1.0.so \
     vendor/xiaomi/sweet/proprietary/system_ext/lib64/vendor.qti.ims.rcsconfig@1.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.ims.rcsconfig@1.0.so \
     vendor/xiaomi/sweet/proprietary/system_ext/lib64/vendor.qti.imsrtpservice@3.0.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/vendor.qti.imsrtpservice@3.0.so \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_adsp_loader.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_adsp_loader.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_apr.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_apr.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_aw882xx.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_aw882xx.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_bolero_cdc.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_bolero_cdc.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_hdmi.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_hdmi.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_machine_talos.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_machine_talos.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_mbhc.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_mbhc.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_native.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_native.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_pinctrl_lpi.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_pinctrl_lpi.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_pinctrl_wcd.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_pinctrl_wcd.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_platform.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_platform.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_q6.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_q6.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_q6_notifier.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_q6_notifier.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_q6_pdr.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_q6_pdr.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_rx_macro.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_rx_macro.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_snd_event.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_snd_event.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_stub.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_stub.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_swr.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_swr.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_swr_ctrl.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_swr_ctrl.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_tx_macro.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_tx_macro.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_usf.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_usf.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_va_macro.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_va_macro.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wcd934x.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wcd934x.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wcd937x.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wcd937x.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wcd937x_slave.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wcd937x_slave.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wcd9xxx.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wcd9xxx.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wcd_core.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wcd_core.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wcd_spi.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wcd_spi.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wglink.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wglink.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wsa881x.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wsa881x.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/audio_wsa_macro.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/audio_wsa_macro.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/exfat.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/exfat.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/fpc1540.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/fpc1540.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/modules.alias:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/modules.alias \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/modules.dep:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/modules.dep \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/modules.load:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/modules.load \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/modules.softdep:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/modules.softdep \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/mpq-adapter.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/mpq-adapter.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/mpq-dmx-hw-plugin.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/mpq-dmx-hw-plugin.ko \
-    vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/modules/qca_cld3_wlan.ko:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/modules/qca_cld3_wlan.ko \
     vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib/soundfx/libvolumelistener.so \
     vendor/xiaomi/sweet/proprietary/product/vendor_overlay/30/lib64/soundfx/libvolumelistener.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/lib64/soundfx/libvolumelistener.so
 
